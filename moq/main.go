1882d7fcd06eccaefa5d178196d6617a2613ed03
shiiyan <shiiyan79@gmail.com>
2025-05-25 19:25:03 +0900

    test: generate moq test

---
+++ moq/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "context"
+
+type Person struct {
+	ID      string
+	Name    string
+	Company string
+	Website string
+}
+
+type PersonStore interface {
+	Get(ctx context.Context, id string) (*Person, error)
+	Create(ctx context.Context, person *Person, confirm bool) error
+}
