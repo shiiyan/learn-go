3d72edf89b30127a162a300bef14b7d277128f2a
shiiyan <shiiyan79@gmail.com>
2025-07-13 14:21:19 +0900

    feat: add mutant to div test

---
+++ mutation-test-ast/div.go
@@ -0,0 +1,15 @@
+package main
+
+import "errors"
+
+var ErrDivideByZero = errors.New("cannot divide by zero")
+
+func divide(divident, divisor int) (int, error) {
+	// mutant 1: reverse if condition
+	if divisor == 0 {
+		return 0, ErrDivideByZero
+	}
+	
+	// mutant 2: change the operator
+	return divident * divisor, nil
+}
